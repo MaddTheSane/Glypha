e9730bb495ea40a899db875f14220d24b3c2c87d
C.W. Betts <computers57@hotmail.com>
2015-05-28 14:25:04 -0600

    Initial OpenAL port.

---
+++ game/GLSoundsOpenAL.cpp
@@ -0,0 +1,29 @@
+//
+//  GLSoundsOpenAL.cpp
+//  Glypha
+//
+//  Created by C.W. Betts on 5/28/15.
+//  Copyright (c) 2015 Kevin Wojniak. All rights reserved.
+//
+
+#include "GLSounds.h"
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+
+
+
+void GL::Sounds::initContext()
+{
+    
+}
+
+void GL::Sounds::load(GL::SoundID which, const unsigned char *buf, unsigned bufLen)
+{
+    
+}
+
+void GL::Sounds::play(GL::SoundID which)
+{
+    
+}
+
