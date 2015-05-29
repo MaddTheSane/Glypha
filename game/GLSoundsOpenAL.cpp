ad05a44d4bfb6edc0c592a9a5c6acfe0ea5c4678
C.W. Betts <computers57@hotmail.com>
2015-05-28 18:56:27 -0600

    More OpenAL work.

--- game/GLSoundsOpenAL.cpp
+++ game/GLSoundsOpenAL.cpp
@@ -9,17 +9,29 @@
 #include "GLSounds.h"
 #include <OpenAL/al.h>
 #include <OpenAL/alc.h>
+#include <ALUT/alut.h>
 
-
+struct Context {
+    ALuint buffers[GL::kMaxSounds];
+    
+    Context() {
+        // Init openAL
+        alutInit(0, NULL);
+        // Clear Error Code (so we can catch any new errors)
+        alGetError();
+        //alGenBuffers(GL::kMaxSounds, buffers);
+    }
+};
 
 void GL::Sounds::initContext()
 {
-    
+    context = new Context;
 }
 
 void GL::Sounds::load(GL::SoundID which, const unsigned char *buf, unsigned bufLen)
 {
-    
+    Context *ctx = static_cast<Context*>(context);
+    ctx->buffers[which] = alutCreateBufferFromFileImage(buf, bufLen);
 }
 
 void GL::Sounds::play(GL::SoundID which)
